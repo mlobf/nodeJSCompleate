34971d9cb22af995e45acf1ee119360f65dadaf2
marcos <marcoslemeborbafilho@gmail.com>
2020-12-29 17:04:54 -0300

    creating my own functions and using on separate files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,10 @@
-const fs = require('fs');
 /*
-fs.writeFileSync(
-    'notes.txt',
-    'My name is Marcos.'
-);
+const add = require('./utils.js')
+const sum = add(1,2);
+console.log(sum);
 */
-/*
-    Challenge: Append a message to notes.txt
+const multiplicacao = require('./challengeOne.js')
 
+const print = multiplicacao(2, 5);
 
-    1.Use appendFileSync to append to the file
-    2.Run the script
-    3.Check your work by opening the file and viewing the appended text
-
-*/
-fs.appendFileSync('notes.txt', '.This is an append note.');
+console.log(print);
