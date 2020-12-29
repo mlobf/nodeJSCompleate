fcf1995a15a3024c7942ccf4114588574108ab9b
marcos <marcoslemeborbafilho@gmail.com>
2020-12-29 16:18:04 -0300

    Using Node to write some text on txt files and append it.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,17 @@
-
-let x = 10;
-let y = 11;
-
-if (x ===10) {
-    console.log('E maior que 10')
-}
-
-console.log("clasf");
-
-
+const fs = require('fs');
+/*
+fs.writeFileSync(
+    'notes.txt',
+    'My name is Marcos.'
+);
+*/
+/*
+    Challenge: Append a message to notes.txt
 
 
+    1.Use appendFileSync to append to the file
+    2.Run the script
+    3.Check your work by opening the file and viewing the appended text
 
-
-
-
-
+*/
+fs.appendFileSync('notes.txt', '.This is an append note.');
