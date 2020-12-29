227721c3bf9426267506910b9a45ff2ff44e0a74
marcos <marcoslemeborbafilho@gmail.com>
2020-12-29 19:04:00 -0300

    validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,8 +3,14 @@ const add = require('./utils.js')
 const sum = add(1,2);
 console.log(sum);
 */
-const multiplicacao = require('./challengeOne.js')
+const multiplicacao = require('./challengeOne.js');
 
+
+
+
+
+
+const validator = require('validator');
 const print = multiplicacao(2, 5);
 
 console.log(print);
