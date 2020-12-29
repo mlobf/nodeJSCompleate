41582093aeee3facdccdaea633bc600b28658064
marcos <marcoslemeborbafilho@gmail.com>
2020-12-29 19:51:52 -0300

    Printing some color on console with chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,13 +4,11 @@ const sum = add(1,2);
 console.log(sum);
 */
 const multiplicacao = require('./challengeOne.js');
-
-
-
-
-
-
 const validator = require('validator');
 const print = multiplicacao(2, 5);
+const chalk = require('chalk');
 
-console.log(print);
+console.log(chalk.green.inverse.bold('Hello My Success!!!'));
+//console.log(print);
+//console.log(validator.isEmail('marcos@gmail.com'));
+//console.log(validator.isURL('www.uol.com.br'));
--- notes-app/challengeOne.js
+++ notes-app/challengeOne.js
@@ -1,5 +1,3 @@
-
-
 const multiplicacao = function (a, b) {
     return a * b;
 };
