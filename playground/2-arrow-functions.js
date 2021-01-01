a0061963a1c0c97f798b176a7be946e5b38cdaba
marcos <marcoslemeborbafilho@gmail.com>
2021-01-01 19:11:01 -0300

    Arrow functions

---
+++ playground/2-arrow-functions.js
@@ -0,0 +1,20 @@
+// Arrow Functions
+console.log('Using tradicional functions approach');
+/* 
+console.log(" Run the script");
+const square = function (x) {
+    return x * x;
+};
+*/
+
+//console.log('Using arrows functions es6');
+
+//const square = (x) => {
+//    return x * x;
+//};
+// Now the short hand syntax of arrow functions;
+
+const square = (x) => x * x;
+
+let quadrado = square(5);
+console.log(quadrado);
