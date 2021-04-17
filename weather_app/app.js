74fdae84beb05f403aed5c0d904d2febeffb8141
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-17 00:49:25 -0300

    Lesson 31 Making Http Requests

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,5 +1,20 @@
 "use strict";
+
+// Basic Async Lesson
+
 console.log("Starting");
 
+setTimeout(() => {
+	console.log("2 second Timer");
+}, 2000);
+
+setTimeout(() => {
+	console.log("O second timer");
+}, 0);
 
 console.log("Stopping");
+/*
+  Call Stack
+  CallBack Queue
+  Event Loop
+*/
