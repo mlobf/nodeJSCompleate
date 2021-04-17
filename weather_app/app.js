6bee139ec3d4240b7e0c6d1aa236b2c4263a615d
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-17 01:29:27 -0300

    Lesson31

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,20 +1,26 @@
 "use strict";
 
+const request = require("request");
+
+const url =
+	"http://api.weatherstack.com/current?access_key=be0d617236c0e7dd1c5bb4bd097fa5fd&&query=37.8267,-122.4233";
+
+request({ url: url }, (error, response) => {
+	const data = JSON.parse(response.body);
+	console.log(data.current);
+});
+
 // Basic Async Lesson
 
-console.log("Starting");
-
-setTimeout(() => {
-	console.log("2 second Timer");
-}, 2000);
-
-setTimeout(() => {
-	console.log("O second timer");
-}, 0);
-
-console.log("Stopping");
 /*
   Call Stack
   CallBack Queue
   Event Loop
+  My Api Key
+  password
+  Futuro@10
+  be0d617236c0e7dd1c5bb4bd097fa5fd
+  http://api.weatherstack.com/
+  A simple request
+  http://api.weatherstack.com/current?access_key=be0d617236c0e7dd1c5bb4bd097fa5fd&&query=37.8267,-122.4233
 */
