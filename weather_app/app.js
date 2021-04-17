650a90aefc7d651b7cd72a8e90a83bd79af2ad02
mlobf <marcoslemeborbafilho@gmail.com>
2021-04-17 14:51:55 -0300

    End Lesson 32 Custom Resquest at weatherstack

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,14 +1,21 @@
 "use strict";
-
+//Stop at 3:33
 const request = require("request");
 
 const url =
 	"http://api.weatherstack.com/current?access_key=be0d617236c0e7dd1c5bb4bd097fa5fd&&query=37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-	const data = JSON.parse(response.body);
-	console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+	let temperature = response.body.current["temperature"];
+	let precip = response.body.current["precip"];
+	let cloudcover = response.body.current["cloudcover"];
+
+	console.log(
+		`Its currently ${temperature} with ${precip} % chance of rain and a cloudcouver of ${cloudcover}`
+	);
 });
+
+// Challenge : Print: Its currently 59 degress out. There is a 0% chance of rain.
 
 // Basic Async Lesson
 
@@ -23,4 +30,4 @@ request({ url: url }, (error, response) => {
   http://api.weatherstack.com/
   A simple request
   http://api.weatherstack.com/current?access_key=be0d617236c0e7dd1c5bb4bd097fa5fd&&query=37.8267,-122.4233
-*/
+  */
